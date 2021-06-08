1e2e57cf86b1dc07ed26caa61f5365c5a5970a51
sielock <aaboarder@gmail.com>
2021-06-08 15:45:03 -0600

    basic styling for timer component

--- src/components/Timer/DurationSetter.js
+++ src/components/Timer/DurationSetter.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import styles from './DurationSetter.module.css';
+import React from "react";
+import styles from "./DurationSetter.module.css";
 
-const DurationSetter = props => {
-    return <div></div>
-}
+const DurationSetter = (props) => {
+  return (
+    <div>
+      <header>
+        <h2>{props.mode}</h2>
+      </header>
+      <div>
+          <button>+</button>
+          <span>10</span>
+          <button>-</button>
+      </div>
+    </div>
+  );
+};
 
 export default DurationSetter;
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,18 +2,22 @@ import React from "react";
 import styles from "./Timer.module.css";
 
 const Timer = () => {
-  return <div>
-  <div>
-      <header>Working</header>
-  </div>
-  <div>
-      25:00
-  </div>
-  <div>
-      <button>Start</button>
-      <button>Stop</button>
-  </div>
-  </div>;
+  return (
+    <div className={styles.timer}>
+      <div className={styles.modeTitle}>
+        <header>
+          <h2>Working</h2>
+        </header>
+      </div>
+      <div className={styles.countdown}>
+        <p>25:00</p>
+      </div>
+      <div className={styles.controls}>
+        <button>Start</button>
+        <button>Stop</button>
+      </div>
+    </div>
+  );
 };
 
 export default Timer;
